79019425b19a42d0f054bcb2854237b7520840e1
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-10-31 17:29:06 +0100

    installing tailwind and first tries, WIP

--- e-shop-react/src/components/Homepage/Homepage.js
+++ e-shop-react/src/components/Homepage/Homepage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react' //importing the useState to hook on the page and useEffect to fetch the products
-import './Homepage.css';
+// import './Homepage.css';
 import axios from "axios";
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/cart'
@@ -38,36 +38,41 @@ const toggle = () => {
 }
 
       return (
+        
 
-          <div className='flex justify-between items-center px-20 py-5'>
-            <h1 className='text-2xl uppercase font-bold mt-10 text-center mb-10'>Let's enjoy shopping !!</h1>
-            {!showModal && <button className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'
-              onClick={toggle}
-            >Cart ({cartItems.length})</button>}
+        <div className='flex flex-col justify-center bg-stone-100'>
 
-        <div className='cards'>
+        <div className='flex justify-between items-center px-20 py-5'>
+          <h1 className='text-2xl uppercase font-bold mt-10 text-center mb-10'>Let's enjoy shopping</h1>
+          {!showModal && <button className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'
+            onClick={toggle}
+          >Cart ({cartItems.length})</button>}
+        </div>
+
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10 place-items-center'>
           { loading ? <h1> loading ....</h1>  : 
         
         products.map((product)  =>  {
 
           return (
 
-            <div key={product.id} className='product-card'>
+            <div key={product.id} className='bg-white shadow-md rounded-lg px-10 py-10'>
               <div className='link-card'>
                 <Link to={`/product/${product.id}`} className='product-link'>
-                <img src={product.image} alt={product.title} className='product-image'  />
+                <img src={product.image} alt={product.title} className='rounded-md h-48'  />
 
-                <div className='product-info'>
+                <div className='mt-4'>
                 
-                  <h1 className='product-title'>{product.title.slice(0, 27)}</h1>
-                  <p className='product-description'>{product.description.slice(0, 35)}...</p>
-                  <p className='product-price'>${product.price}</p>
+                  <h1 className='text-lg uppercase font-bold'>{product.title.slice(0, 27)}</h1>
+                  <p className='mt-2 text-gray-600 text-sm'>{product.description.slice(0, 35)}...</p>
+                  <p className='mt-2 text-gray-600'>${product.price}</p>
                   
                 </div>
                 </Link>
               </div>
-              <div className='product-add-div'>
-                <button onClick={() => addToCart(product)}  className='product-add-button'>Add to cart</button>
+              <div className='mt-6 flex justify-between items-right'>
+                <button onClick={() => addToCart(product)}  
+                className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'>Add to cart</button>
               </div>
               
             </div>
@@ -78,7 +83,6 @@ const toggle = () => {
         }
       
       </div>
-      
       
       <Cart showModal={showModal} toggle={toggle} />
       </div>
