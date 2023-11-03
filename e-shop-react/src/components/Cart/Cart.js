e04f64cb163df72985335984bdb54781be1fd8ab
Kathleen-Vierstraete <ctrlkate89@gmail.com>
2023-11-03 17:20:59 +0100

    wip responsive with tailwind on cart

--- e-shop-react/src/components/Cart/Cart.js
+++ e-shop-react/src/components/Cart/Cart.js
@@ -11,8 +11,8 @@ const Cart = ({showModal, toggle}) => {
     return (
         showModal && (
 
-        <div className="flex-col flex items-center fixed inset-0 left-1/4 bg-white dark:bg-black gap-8  p-10  text-black dark:text-white font-normal uppercase text-sm">
-            <h1 className="text-2xl font-bold">Cart</h1>
+        <div className="flex-col flex items-center fixed inset-0 sm:left-1/4 bg-white dark:bg-black gap-8  p-10  text-black dark:text-white font-normal uppercase text-sm ">
+            <h1 className=" sm:text-2xl font-bold">Cart</h1>
             <div className="absolute right-16 top-10">
                 <button
                 className="px-4 py-2 bg-indigo-800 text-white text-xs font-bold uppercase rounded hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700"
@@ -21,14 +21,14 @@ const Cart = ({showModal, toggle}) => {
                 </button>
             </div>
 
-            <div className="flex flex-col gap-4">
+            <div className="grid sm:grid-cols-1 gap-4 px-10 place-items-center space-x-4 pb-10">
             {cartItems.map((item) => (
                 <div className="flex justify-between items-center" key={item.id}>
                     <div className="flex gap-4">
-                        <img src={item.image} alt={item.title} className="rounded-md w-24 h-24" />
+                        <img src={item.image} alt={item.title} className="rounded-md sm:w-24 sm:h-24 w-14 h-14" />
                         <div className="flex gap-8 justify-center">
-                            <h1 className="text-lg font-bold">{item.title}</h1>
-                            <p className="text-gray-600">{item.price}</p>
+                            <h1 className="sm:text-lg font-bold">{item.title.slice(0, 20)}</h1>
+                            <p className="text-gray-600">${item.price}</p>
                         </div>
                     </div>
 
